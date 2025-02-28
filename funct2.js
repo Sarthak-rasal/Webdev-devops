8afe1a3d1683758596b9df33283318697f4fdbca
Sarthak Rasal <varshanr75@gmail.com>
2025-02-28 18:41:46 +0530

    added and understood callbacks

--- funct2.js
+++ funct2.js
@@ -1,17 +1,18 @@
-// This function uses no callback functions
-
-function calculateArithematic(a, b, type) {
-  if (type == "sum") {
-    const value = sum(a, b);
-    return value;
-    //return a + b;
-  }
-  if (type == "sub") {
-    const value = sub(a, b);
-    return value;
-    //return a - b;
-  }
+function calculateArithmetic(a, b, arithmeticFinalFun) {
+  const ans = arithmeticFinalFun(a, b);
+  return ans;
 }
+//   if (type == "sum") {
+//     // const value = sum(a, b);
+//     // return value;
+//     //return a + b;
+//   }
+//   if (type == "sub") {
+//     const value = sub(a, b);
+//     return value;
+//     //return a - b;
+//   }
+// }
 
 function sum(a, b) {
   return a + b;
@@ -21,5 +22,5 @@ function sub(a, b) {
   return a - b;
 }
 
-const value = calculateArithematic(1, 2, "sum");
+const value = calculateArithmetic(1, 2, sum);
 console.log(value);
